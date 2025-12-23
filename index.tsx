2b60628f252eb72143e0de2f21eb9cfb756a1062
Baiyang Yao <43796777+yaohuangguan@users.noreply.github.com>
2025-12-23 15:05:26 +0800

    update

--- index.tsx
+++ index.tsx
@@ -4,13 +4,14 @@ import App from './App';
 import { LanguageProvider } from './i18n/LanguageContext';
 import { BrowserRouter } from 'react-router-dom';
 import { inject } from '@vercel/analytics';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 // Initialize Vercel Web Analytics
 inject();
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error('Could not find root element to mount to');
 }
 
 const root = ReactDOM.createRoot(rootElement);
@@ -19,6 +20,7 @@ root.render(
     <BrowserRouter>
       <LanguageProvider>
         <App />
+        <SpeedInsights />
       </LanguageProvider>
     </BrowserRouter>
   </React.StrictMode>
